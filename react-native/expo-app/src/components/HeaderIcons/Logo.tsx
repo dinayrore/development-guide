3605c23c6c1634af2269851b8d8060ce5cf31e22
Kris <horn.kristeen@gmail.com>
2023-08-16 14:49:42 -0400

    update logo

--- react-native/expo-app/src/components/HeaderIcons/Logo.tsx
+++ react-native/expo-app/src/components/HeaderIcons/Logo.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Image } from "react-native";
-import { styles } from "./styles";
 
 const Logo = () => {
   return <Image source={require("assets/logo.png")} />;
